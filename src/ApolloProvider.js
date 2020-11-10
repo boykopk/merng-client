adff2cd2631c18d307f5de4205fdf6f85d13d04b
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2020-11-10 13:29:06 +0200

    new heroku adres

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'https://dry-dawn-63766.herokuapp.com/',
+  uri: 'https://boiling-forest-45586.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
