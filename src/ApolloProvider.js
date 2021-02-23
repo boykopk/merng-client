214e808eb8f79b4171d5d14b4e774df07cf86bb7
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2021-02-23 15:48:36 +0200

    add new server

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'https://blooming-waters-60237.herokuapp.com/',
+  uri: 'https://merng-new.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
